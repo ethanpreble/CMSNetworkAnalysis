45682fb84a8cccedd96234711a63d7438b50483e
ethanpreble <ethanpreble@gmail.com>
2015-03-06 17:17:15 -0800

    remove dollar sign working

--- CMS/project_data/removeDollarSign.py
+++ CMS/project_data/removeDollarSign.py
@@ -7,25 +7,18 @@ with open('Pharma_Doctor_Payment_data.csv', 'rb') as csvfile:
 	fieldnames = reader.fieldnames
 	print fieldnames
 
+	i=0
 	with open('Pharma_Doctor_Payment_data_no_dollar_sign.csv', 'wb') as f:
 		writer = csv.DictWriter(f, fieldnames=fieldnames)
 		writer.writeheader()	
 		for row in reader:
 			try:
-				newRow = []
-				for fieldname in fieldnames:
-					if fieldname != 'Total_Amount_of_Payment_USDollars':
-						newRow.append({'fieldname':row[fieldname]})
-					else:
-						newRow.append({'fieldname':row[fieldname][1:]})
-				#print row['Total_Amount_of_Payment_USDollars']
-				#dollars = row['Total_Amount_of_Payment_USDollars']
-				#dollars = dollars[1:]
-				#writer.writerow({'Total_Amount_of_Payment_USDollars':dollars})
-				write.writerow(newRow)
+				row['Total_Amount_of_Payment_USDollars'] = row['Total_Amount_of_Payment_USDollars'][1:] 
+				writer.writerow(row)
 			except Exception as e:
 				pass
+				
+			i +=1
+			if(i%100000 == 0):
+				print i
 		
-		#			stateWriters[states.index(row['Recipient_State'])].writerow(row['Physician_Profile_ID'],row['Total_Amount_of_Payment_USDollars'].replace("$",""))
-		#    writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
-
