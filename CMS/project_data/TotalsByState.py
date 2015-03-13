f89c8a9c130b04d30a6cb28783ad3977c32f42ad
ethanpreble <ethanpreble@gmail.com>
2015-03-13 14:59:46 -0700

    adding hospitals and doctor data for sb

--- CMS/project_data/TotalsByState.py
+++ CMS/project_data/TotalsByState.py
@@ -83,5 +83,13 @@ f.close()
 #			for key in total:
 #				writer3.writerow([key,str(total[key])])
 		
+		#with open('names.csv', 'w') as csvfile:
+   # fieldnames = ['first_name', 'last_name']
+    #writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    #writer.writeheader()
+    #writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+    #writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
+    #writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'}
 		
 		
